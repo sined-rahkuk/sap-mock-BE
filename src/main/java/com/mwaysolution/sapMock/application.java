d490f60d6ba88d5e33bcb377ee93fc52920eb5b2
Anton Zezul <anton.zezul@student.tuke.sk>
2019-10-28 10:57:04 +0100

    create service component

--- src/main/java/com/mwaysolution/sapMock/application.java
+++ src/main/java/com/mwaysolution/sapMock/application.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.mwaysolution.sapMock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+class SapMockApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SapMockApplication.class, args);
 	}
 
 }
